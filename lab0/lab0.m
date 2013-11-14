b7de164e807f1b330f9b29d9582ebbf93039eebb
gianitobia <toby@BookTob.local>
2013-11-14 21:48:20 +0100

    commentato lab0

--- lab0/lab0.m
+++ lab0/lab0.m
@@ -1,51 +1,60 @@
 clear;
 close all;
 
-%%%esercizio 1
+%%%esercizio 1 creare un immagine con un onda sinusoidale orizzontale
 maxx = 255;
 maxy = maxx;
 y = 0:maxy;
 f = 8;
-s = cos(2*pi*f*y/(maxy+1));   % s is a sine oscillating between -1 and 1
-sinimage = ones(maxx+1,1)*s;   % copy a row to build an image
-imshow(sinimage, []);   % y increases right, x increases downward
+%creiamo un vettore con i valori della funzione cos
+s = cos(2*pi*f*y/(maxy+1));  
+%moltiplichiamo il vettore s per un vettore di 1 cosi da creare una
+%matrice con tutte righe identiche ad s
+sinimage = ones(maxx+1,1)*s;   
+imshow(sinimage, []);   
 colormap(gray);
 
-%%%%%%esercizio 2
+%%%esercizio 2 creare un immagine con sinusoidi a frequenze differenti
+%%%sulle tre componenti cromatiche
+%costruzione di una matrice tridimensionale per avere un immagine a colori
 sinimage=zeros(maxx+1,maxy+1,3);
+%sinusoidi per i 3 canali dell'immagine con frequenze pari a 6,8,4
 r=cos(2*pi*6*y/(maxy+1));
 b=cos(2*pi*8*y/(maxy+1));
 g=cos(2*pi*4*y/(maxy+1));
 sinimage(:,:,1)=ones(maxx+1,1)*r;
 sinimage(:,:,2)=ones(maxx+1,1)*g;
 sinimage(:,:,3)=ones(maxx+1,1)*b;
+figure, imshow(sinimage, []);
 
-figure;
-imshow(sinimage, []);
-
-%%%%%esercizio 3 rettangolo
+%%%esercizio 3 costruzione di un immagine nera con un rettangolo bianco
+%%%al centro
 img=zeros(maxx,maxy);
 img(maxx/2-30:maxx/2+30,maxy/2-50:maxy/2+50)=255*ones(61,101);
-
 imshow(img);
 
-%%%%%esercizio 3 cerchio
+%%%esercizio 3 costruzione di un immagine nera con un cerchio centrato in
+%%%(cx,cy) e raggio r
+
 img=zeros(maxx,maxy);
 cx=100;
 cy=100;
 r=50;
 for i=1:maxx
     for j=1:maxy
+        %selezioniamo solo i pixel che hanno distanza dal centro del
+        %cerchio minore del raggio del cerchio
         dist=sqrt((cx-i)^2+(cy-j)^2);
         if dist<r
             img(i,j)=255;
         end;
     end;
 end;
-
 figure;imshow(img);
 
 figure();
+%blockproc è una funzione che scompone la matrice in blocchi nxn e applica
+%una certa func su ogni blocco separatamente e poi li ricompone
 %%%esercizio 13
 fun = @(block_struct) mean(mean(block_struct.data)).*ones(size(block_struct.data));
 I = imread('../lena512.bmp');
